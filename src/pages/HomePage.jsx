26777bd193bbc39177e4659fce6df2c512747446
Jake Bowden <jbowden@jahnelgroup.com>
2024-06-28 16:16:41 -0400

    Added basic homepage styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
+//import backgroundImage from "./path-to-your-image.jpg";
 
 const HomePage = () => {
-    return(
-        <h1>HOME PAGE</h1>
-    )
-}
+  return (
+    <div className="home-page">
+      <h1>HOME PAGE</h1>
+      <Link to="/shop">
+        <button className="shop-now-button">Shop Now</button>
+      </Link>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
