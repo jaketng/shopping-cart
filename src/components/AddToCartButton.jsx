9fe86fae62d0887aec8cb575e1063088c56aa9b3
Jake Bowden <jbowden@jahnelgroup.com>
2024-06-28 11:24:12 -0400

    Improved styling of add to cart button and made webpage responsive

---
+++ src/components/AddToCartButton.jsx
@@ -0,0 +1,9 @@
+const AddToCartButton = () => {
+  return (
+    <>
+      <button className="add-to-cart-button">+ Add to cart</button>
+    </>
+  );
+};
+
+export default AddToCartButton;
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,16 +1,33 @@
+import { useState } from "react";
+import AddToCartButton from "./AddToCartButton.jsx";
 
 const ProductCard = ({ product }) => {
+  const [quantity, setQuantity] = useState(0);
+
+  const handleIncrement = () => {
+    setQuantity((prevQuantity) => prevQuantity + 1);
+  };
+
+  const handleDecrement = () => {
+    setQuantity((prevQuantity) => (prevQuantity > 0 ? prevQuantity - 1 : 0));
+  };
+
+  const handleInput = (e) => {
+    const value = parseInt(e.target.value);
+    setQuantity(value >= 0 ? value : 0);
+  };
+
   return (
     <div className="product-card">
       <h2>{product.title}</h2>
       <img src={product.image} alt={product.title} />
-      <p>{product.price}</p>
-      <div>
-        <button>-</button>
-        <input type="number" defaultValue="1" />
-        <button>+</button>
+      <p>${product.price}</p>
+      <div className="quantity-controls">
+        <button onClick={handleDecrement}>-</button>
+        <input type="number" value={quantity} onChange={handleInput} min="1" />
+        <button onClick={handleIncrement}>+</button>
       </div>
-      <button>Add to Cart</button>
+      <AddToCartButton quantity={quantity} />
     </div>
   );
 };
