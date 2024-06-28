d188e8a7499673cd4ad76ad60ab71499aef5567b
Jake Bowden <jbowden@jahnelgroup.com>
2024-06-28 09:48:49 -0400

    Improved styling and added cart page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,34 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { ShoppingCart } from "phosphor-react";
 
 const Navbar = () => {
   return (
     <>
-    <nav>
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/shop'>Shop</Link></li>
-      </ul>
-    </nav>
+      <nav>
+        <div className="nav-header">
+          <Link to="/">
+            <h1>MyStore</h1>
+          </Link>
+        </div>
+
+        <div className="nav-links">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/shop">Shop</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="cart-icon">
+          <Link to="/cart">
+            {" "}
+            <ShoppingCart size={32} />
+          </Link>
+        </div>
+      </nav>
     </>
   );
 };
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,14 @@
-import { Routes, Route } from 'react-router-dom';
-import HomePage from '../pages/HomePage.jsx';
-import ShopPage from '../pages/ShopPage.jsx';
+import { Routes, Route } from "react-router-dom";
+import HomePage from "../pages/HomePage.jsx";
+import ShopPage from "../pages/ShopPage.jsx";
+import CartPage from "../pages/CartPage.jsx";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/shop" element={<ShopPage />} />
+      <Route path="/cart" element={<CartPage />} />
     </Routes>
   );
 };
